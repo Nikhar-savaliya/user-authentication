c76e782601e5f8bd695590e2ff2357e30cefd676
Nikhar S. <127284370+Nikhar-savaliya@users.noreply.github.com>
2024-06-21 10:36:39 +0530

    add welcome page

--- login.php
+++ login.php
@@ -97,7 +97,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
   <meta charset="UTF-8">
   <title>Sign in</title>
-  <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/cosmo/bootstrap.min.css" rel="stylesheet" integrity="sha384-qdQEsAI45WFCO5QwXBelBe1rR9Nwiss4rGEqiszC+9olH1ScrLrMQr1KmDR964uZ" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <style>
     .wrapper {
       width: 500px;
--- welcome.php
+++ welcome.php
@@ -1,11 +1,50 @@
+<?php
+// Initialize session
+session_start();
+
+if (!isset($_SESSION['loggedin']) && $_SESSION['loggedin'] !== false) {
+	header('location: login.php');
+	exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+	<meta charset="UTF-8">
+	<title>Welcome</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<style>
+		.wrapper {
+			width: 500px;
+			padding: 20px;
+		}
+
+		.wrapper h2 {
+			text-align: center
+		}
+
+		.wrapper form .form-group span {
+			color: red;
+		}
+	</style>
 </head>
+
 <body>
-    welcome
+
+	<!-- Your desire code -->
+	<main>
+		<section class="container wrapper">
+			<div class="page-header">
+				<h4 class="">You are currently logged in as <?php echo $_SESSION['username']; ?></h4>
+			</div>
+
+			<a href="password_reset.php" class="btn btn-block btn-outline-warning">Reset Password</a>
+			<a href="logout.php" class="btn btn-block btn-outline-danger">Sign Out</a>
+		</section>
+	</main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
 </body>
+
 </html>
